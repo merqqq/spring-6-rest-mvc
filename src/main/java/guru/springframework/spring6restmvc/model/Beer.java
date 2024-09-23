58dac537bb5a16c43e89fd4f0f920f817f43d38c
merqqq <marcinchajczyk@gmail.com>
2024-09-23 14:10:42 +0200

    53 - Project Lombok Builders - add BeerService & add BeerBuilder & add missing BeerStyle enum

--- src/main/java/guru/springframework/spring6restmvc/model/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/model/Beer.java
@@ -1,15 +1,17 @@
 package guru.springframework.spring6restmvc.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Builder
 @Data
 public class Beer {
 
-    private UUID uuid;
+    private UUID id;
     private Integer version;
     private String beerName;
     private BeerStyle beerStyle;
