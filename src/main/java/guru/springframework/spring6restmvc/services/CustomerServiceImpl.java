dbbe47446c6ab0fd8e405f5065cdf9c619a4654b
merqqq <marcinchajczyk@gmail.com>
2024-09-25 10:18:04 +0200

    65 - Spring MVC Rest Services - Assignment - HTTP PUT with Spring MVC - add updateCustomerById using PUT

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -75,4 +75,13 @@ public class CustomerServiceImpl implements CustomerService {
 
         return savedCustomer;
     }
+
+    @Override
+    public void updateCustomerById(UUID customerId, Customer customer) {
+
+        Customer existing = customerMap.get(customerId);
+        existing.setName(customer.getName());
+
+        customerMap.put(existing.getId(), existing);
+    }
 }
